a9ee6c29cec3753e353af5626f8c900cf469b6a1
James Baker <jamesbak@microsoft.com>
2018-01-30 13:44:58 -0800

    Update to SourceData function to bind to trigger blob

--- SourceData/index.js
+++ SourceData/index.js
@@ -1,18 +1,17 @@
 var tar = require('tar');
+var stream = require('stream');
 var azure_storage = require('azure-storage');
 
-module.exports = function (context) {
+module.exports = function (context, sourceArchive) {
     var blobService = azure_storage.createBlobService(process.env.DataConnection);
-    blobService.createReadStream(process.env.SourceDataContainer, `zipped/${context.bindingData.name}`)
-        .pipe(tar.x())
+    var sourceStream = new stream.PassThrough();
+    sourceStream.end(sourceArchive);
+    sourceStream.pipe(tar.x())
         .on('entry', (entry) => {
             if (entry.type === 'File') {
                 entry.pipe(
                     blobService.createWriteStreamToBlockBlob(process.env.SourceDataContainer, process.env.UnzipLocation + entry.path)
-                )
-                .on('end', () => {
-                    context.log(`Completed writing blob to: ${process.env.SourceDataContainer}/${process.env.UnzipLocation + entry.path}`);
-                });
+                );
             }
         })
         .on('end', () => {
