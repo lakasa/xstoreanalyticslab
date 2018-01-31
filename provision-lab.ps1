cbd5c4fa00fec5cbef1071b4f498d16a905e0ba0
James Baker <jamesbak@microsoft.com>
2018-01-31 14:36:56 -0800

    Omitted clientId & clientSecret from provisioning logic to allow participants to create during lab

--- provision-lab.ps1
+++ provision-lab.ps1
@@ -1,11 +1,12 @@
 param(
-    [Int32]$increment,
-    [string]$upn,
-    [string]$clientSecret
+    [Parameter(Mandatory=$true)][Int32]$increment,
+    [Parameter(Mandatory=$true)][string]$upn,
+    [Parameter(Mandatory=$true)][string]$dataBricksToken,
+    [string]$location = "eastus"
 )
 
 $baseName = $increment.ToString("xstorelab0000")
 $user = Get-AzureRmADUser -Mail $upn
-New-AzureRmResourceGroup -Name $baseName -Location eastus
-New-AzureRmResourceGroupDeployment -Name xstoreilldeploy1 -ResourceGroupName $baseName -Mode Incremental -TemplateFile .\azuredeploy.json -baseName $baseName -userPrincipalId $user.Id -dataBricksUsername jamesbak@microsoft.com -dataBricksToken dapi2f292e228d75afb9f911a6fd9f39dc43 -clientSecret $clientSecret
+New-AzureRmResourceGroup -Name $baseName -Location $location
+New-AzureRmResourceGroupDeployment -Name xstoreilldeploy1 -ResourceGroupName $baseName -Mode Incremental -TemplateFile .\azuredeploy.json -baseName $baseName -userPrincipalId $user.Id -dataBricksUsername jamesbak@microsoft.com -dataBricksToken $dataBricksToken
 New-AzureRmRoleAssignment -ObjectId $user.Id -ResourceGroupName xstoreilldb -ResourceName xstoreill -ResourceType "Microsoft.Databricks/workspaces" -RoleDefinitionName Contributor
