c90bade3afa8f44f2b840eb143e40603233cafe5
James Baker <jamesbak@microsoft.com>
2018-01-31 23:13:10 -0800

    Final updates to lab

--- provision-lab.ps1
+++ provision-lab.ps1
@@ -10,3 +10,7 @@ $user = Get-AzureRmADUser -Mail $upn
 New-AzureRmResourceGroup -Name $baseName -Location $location
 New-AzureRmResourceGroupDeployment -Name xstoreilldeploy1 -ResourceGroupName $baseName -Mode Incremental -TemplateFile .\azuredeploy.json -baseName $baseName -userPrincipalId $user.Id -dataBricksUsername jamesbak@microsoft.com -dataBricksToken $dataBricksToken
 New-AzureRmRoleAssignment -ObjectId $user.Id -ResourceGroupName xstoreilldb -ResourceName xstoreill -ResourceType "Microsoft.Databricks/workspaces" -RoleDefinitionName Contributor
+$account = Get-AzureRmStorageAccount -ResourceGroupName $baseName -Name $baseName
+New-AzureStorageContainer -Context $account.Context -Name sourcedata
+New-AzureStorageContainer -Context $account.Context -Name outputdata
+New-AzureStorageContainer -Context $account.Context -Name archivaldata
